dbd2b99d53c41d8adc601588a178b3a73366fd04
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-05-15 12:26:56 +0200

    feat: add basketIdent and ipAddress options for categories and packages

--- src/services/packages.ts
+++ src/services/packages.ts
@@ -7,11 +7,20 @@ export class PackagesService {
   /**
    * Get all packages
    *
+   * @param options Options
+   * @param options.basketIdent Provide the identifier of the basket
+   * @param options.ipAddress An IPv4 address can be provided with authenticated requests
    * @returns An array of packages
    */
-  public async getPackages(): Promise<Package[]> {
+  public async getPackages(options?: {
+    basketIdent?: string;
+    ipAddress?: string;
+  }): Promise<Package[]> {
     const response = await this.client.context.axios.get(
-      `${this.client.context.accountsEndpoint}/packages`
+      `${this.client.context.accountsEndpoint}/packages`,
+      {
+        params: options,
+      }
     );
 
     return this.client.handleResponse(response);
@@ -20,12 +29,24 @@ export class PackagesService {
   /**
    * Get a package
    *
-   * @param id The package ID
+   * @param id The package id
+   * @param options Options
+   * @param options.basketIdent Provide the identifier of the basket
+   * @param options.ipAddress An IPv4 address can be provided with authenticated requests
    * @returns The package
    */
-  public async getPackage(id: Package["id"]): Promise<Package> {
+  public async getPackage(
+    id: Package["id"],
+    options?: {
+      basketIdent?: string;
+      ipAddress?: string;
+    }
+  ): Promise<Package> {
     const response = await this.client.context.axios.get(
-      `${this.client.context.accountsEndpoint}/packages/${id}`
+      `${this.client.context.accountsEndpoint}/packages/${id}`,
+      {
+        params: options,
+      }
     );
 
     return this.client.handleResponse(response);
