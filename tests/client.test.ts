4a994ac505f7cb34d23525c32c861eba8712459d
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-05-15 09:42:54 +0200

    test: trigger workflow

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -2,7 +2,7 @@ import { TebexHeadlessClient } from "../src";
 
 describe("TebexHeadlessClient", () => {
   test("should be instantiated with a webstore identifier", () => {
-    const client = new TebexHeadlessClient("mock_identifier");
-    expect(client.getIdentifier()).toBe("mock_identifier");
+    const client = new TebexHeadlessClient("mock-identifier");
+    expect(client.getIdentifier()).toBe("mock-identifier");
   });
 });
