6a256e143ccdda0b493ec39ab83b41775711822d
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-05-15 10:31:32 +0200

    fix: jest error in client.test.ts

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -1,8 +1,9 @@
 import { TebexHeadlessClient } from "../src";
 
+const client = new TebexHeadlessClient("mock-identifier");
+
 describe("TebexHeadlessClient", () => {
   test("should be instantiated with a webstore identifier", () => {
-    const client = new TebexHeadlessClient("mock-identifier");
     expect(client.context.identifier).toBe("mock-identifier");
   });
 });
